646179d7c8728b381f3a00310ab31c6e6ee1fe93
jesspsilva <jps936@hotmail.com>
2024-06-04 13:12:26 +0100

    chore: add mui component library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Metrics Dashboard",
-  description: "Metrics dashboard to show several data",
+	title: "Metrics Dashboard",
+	description: "Metrics dashboard to show several data",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+			</body>
+		</html>
+	);
 }
