5932c763ddc4ebab2c04abd1e5b76ab4fd604b3d
jesspsilva <jps936@hotmail.com>
2024-06-04 13:14:12 +0100

    chore: improve app styles

--- src/app/components/Table/Table.tsx
+++ src/app/components/Table/Table.tsx
@@ -1,39 +1,35 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import Badge from '@components/Badge/Badge';
+import Badge from "@components/Badge/Badge";
 
 const formatNumber = (value: number, type: MetricsType): string => {
-  switch (type.toLowerCase()) {
-    case 'number':
-      return new Intl.NumberFormat().format(value);
-    case 'percentage':
-      return new Intl.NumberFormat('pt-PT', { style: 'percent' }).format(value); 
-    case 'secs':
-      return new Intl.NumberFormat('pt-PT', { style: 'unit', unit: 'second' }).format(value);
-    case 'hours':
-      return new Intl.NumberFormat('pt-PT', { style: 'unit', unit: 'hour' }).format(value);
-    default:
-      return value.toString();
-  }
+	switch (type.toLowerCase()) {
+		case "number":
+			return new Intl.NumberFormat().format(value);
+		case "percentage":
+			return new Intl.NumberFormat("pt-PT", { style: "percent" }).format(value);
+		case "secs":
+			return new Intl.NumberFormat("pt-PT", {
+				style: "unit",
+				unit: "second",
+			}).format(value);
+		case "hours":
+			return new Intl.NumberFormat("pt-PT", {
+				style: "unit",
+				unit: "hour",
+			}).format(value);
+		default:
+			return value.toString();
+	}
 };
 
 const TableWrapper = styled.table`
-  width: 100%;
-  border-collapse: collapse;
+	width: 100%;
+	border-collapse: collapse;
 `;
 
 const TableHead = styled.thead`
   border-bottom: 1px solid var(--light-gray);
-
-  th {
-    padding: 20px;
-
-    text-align: left;
-    color: var(--gray);
-    font-weight: 500;
-    font-size: 1rem;
-    line-height: 1.5;
-  }
 
   @media (max-width: 768px) {´
     border: none;
@@ -47,69 +43,92 @@ const TableHead = styled.thead`
   }
 `;
 
+const TableHeader = styled.th`
+	padding: 20px;
+
+	text-align: left;
+	color: var(--gray);
+	font-weight: 500;
+	font-size: 1rem;
+	line-height: 1.5;
+`;
+
 const TableRow = styled.tr`
-  @media (max-width: 768px) {
-    display: block;
-    margin-bottom: 4rem;
-    border: 1px solid var(--light-gray);
-    box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.05);
-  }
+	@media (max-width: 768px) {
+		display: block;
+		border: 1px solid var(--light-gray);
+		box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.05);
+
+		&:not(:last-of-type) {
+			margin-bottom: 4rem;
+		}
+	}
 `;
 
 const TableData = styled.td`
-  font-weight: 400;
-  padding: 20px;
-  text-align: left;
-  font-size: 1rem;
-  color: var(--dark-blue);
+	font-weight: 400;
+	padding: 20px;
+	text-align: left;
+	font-size: 1rem;
+	color: var(--dark-blue);
+	line-height: 1.5;
 
-  border-bottom: 1px solid var(--light-gray);
+	border-bottom: 1px solid var(--light-gray);
 
-  &:first-child {
-    text-transform: capitalize;
-  }
+	&:first-child {
+		text-transform: capitalize;
+	}
 
-  @media (max-width: 768px) {
-    display: block;
-    font-size: 0.9em;
-    text-align: right;
+	@media (max-width: 768px) {
+		display: flex;
+		justify-content: space-between;
+		gap: 10px;
+		padding: 12px;
 
-    &::after {
-      content: attr(data-label);
-      float: left;
-      font-weight: bold;
-    }
+		font-size: 0.9em;
+		text-align: right;
 
+		&::before {
+			content: attr(data-label);
+			float: left;
+			font-weight: bold;
+		}
 
-    &:last-child {
-      border-bottom: 0;
-    }
-  }
+		&:last-child {
+			border-bottom: 0;
+		}
+	}
 `;
 
-export default function Table({data}: {data: MetricsData[]}) {
-  return (
-    <TableWrapper>
-      <TableHead>
-        <tr>
-          <th>Label</th>
-          <th>Value</th>
-          <th>Description</th>
-          <th>Category</th>
-        </tr>
-      </TableHead>
-      <tbody>
-        {data.map((item) => (
-          <TableRow key={item.id}>
-            <TableData data-label="Label">{item.label ? item.label : '-'}</TableData>
-            <TableData data-label="Value">{item.value ? formatNumber(item.value, item.type) : '-'}</TableData>
-            <TableData data-label="Description">{item.description ? item.description : '-'}</TableData>
-            <TableData data-label="Category">
-              {item.category ? <Badge label={item.category} /> : '-'}
-            </TableData>
-          </TableRow>
-        ))}
-      </tbody>
-    </TableWrapper>
-  )
-};
+export default function Table({ data }: { data: MetricsData[] }) {
+	return (
+		<TableWrapper>
+			<TableHead>
+				<tr>
+					<TableHeader>Label</TableHeader>
+					<TableHeader>Value</TableHeader>
+					<TableHeader>Description</TableHeader>
+					<TableHeader>Category</TableHeader>
+				</tr>
+			</TableHead>
+			<tbody>
+				{data.map((item) => (
+					<TableRow key={item.id}>
+						<TableData data-label="Label">
+							{item.label ? item.label : "-"}
+						</TableData>
+						<TableData data-label="Value">
+							{item.value ? formatNumber(item.value, item.type) : "-"}
+						</TableData>
+						<TableData data-label="Description">
+							{item.description ? item.description : "-"}
+						</TableData>
+						<TableData data-label="Category">
+							{item.category ? <Badge label={item.category} /> : "-"}
+						</TableData>
+					</TableRow>
+				))}
+			</tbody>
+		</TableWrapper>
+	);
+}
