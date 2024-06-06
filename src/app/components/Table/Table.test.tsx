6fce223ddbaa02f1c8f40efe5aa9fd2e18408a43
jesspsilva <jps936@hotmail.com>
2024-06-06 19:27:48 +0100

    test: update table tests

--- src/app/components/Table/Table.test.tsx
+++ src/app/components/Table/Table.test.tsx
@@ -5,18 +5,20 @@ import { metricsData as data } from "@/app/utils/__mocks__/tests-data";
 
 describe("Table", () => {
   it("should render correctly", () => {
-    const { asFragment } = render(<Table data={data} />);
+    const { asFragment } = render(
+      <Table data={data} onClick={() => {}} enableFiltering={false} />,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("should render the correct number of rows", () => {
-    render(<Table data={data} />);
+    render(<Table data={data} onClick={() => {}} enableFiltering={false} />);
     const rows = screen.getAllByRole("row");
     expect(rows).toHaveLength(data.length + 1);
   });
 
   it("should render the correct number of columns", () => {
-    render(<Table data={data} />);
+    render(<Table data={data} onClick={() => {}} enableFiltering={false} />);
     const rows = screen.getAllByRole("row");
     const columns = rows[0].querySelectorAll("th");
     expect(columns).toHaveLength(4);
@@ -24,7 +26,9 @@ describe("Table", () => {
 
   describe("when table label is available", () => {
     it("should render the label", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const label = container.querySelectorAll('[data-label="Label"]');
       expect(label[0]).toHaveTextContent("oee");
       expect(label[1]).toHaveTextContent("Speed loss");
@@ -35,7 +39,9 @@ describe("Table", () => {
 
   describe("when table label is not available", () => {
     it("should render a dash", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const label = container.querySelectorAll('[data-label="Label"]');
       expect(label[4]).toHaveTextContent("-");
     });
@@ -43,7 +49,9 @@ describe("Table", () => {
 
   describe("when table value is a percentage", () => {
     it("should format the number correctly", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const value = container.querySelectorAll('[data-label="Value"]')[0];
       expect(value).toHaveTextContent("68%");
     });
@@ -51,7 +59,9 @@ describe("Table", () => {
 
   describe("when table value is a number", () => {
     it("should format the number correctly", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const value = container.querySelectorAll('[data-label="Value"]')[1];
       expect(value).toHaveTextContent("-10.5");
     });
@@ -59,7 +69,9 @@ describe("Table", () => {
 
   describe("when table value is in seconds", () => {
     it("should format the number correctly", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const value = container.querySelectorAll('[data-label="Value"]')[2];
       expect(value).toHaveTextContent("2280 s");
     });
@@ -67,7 +79,9 @@ describe("Table", () => {
 
   describe("when table value is in hours", () => {
     it("should format the number correctly", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const value = container.querySelectorAll('[data-label="Value"]')[3];
       expect(value).toHaveTextContent("8 h");
     });
@@ -75,7 +89,9 @@ describe("Table", () => {
 
   describe("when table value is not available", () => {
     it("should render a dash", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const value = container.querySelectorAll('[data-label="Value"]')[4];
       expect(value).toHaveTextContent("-");
     });
@@ -83,7 +99,9 @@ describe("Table", () => {
 
   describe("when table description is available", () => {
     it("should render the description", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const description = container.querySelectorAll(
         '[data-label="Description"]',
       );
@@ -100,7 +118,9 @@ describe("Table", () => {
 
   describe("when table description is not available", () => {
     it("should render a dash", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const description = container.querySelectorAll(
         '[data-label="Description"]',
       );
@@ -110,7 +130,9 @@ describe("Table", () => {
 
   describe("when table category is available", () => {
     it("should render the category", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const category = container.querySelectorAll('[data-label="Category"]');
       expect(category[0].querySelector("span")).toHaveTextContent("efficiency");
       expect(category[1].querySelector("span")).toHaveTextContent("efficiency");
@@ -121,9 +143,34 @@ describe("Table", () => {
 
   describe("when table category is not available", () => {
     it("should render a dash", () => {
-      const { container } = render(<Table data={data} />);
+      const { container } = render(
+        <Table data={data} onClick={() => {}} enableFiltering={false} />,
+      );
       const category = container.querySelectorAll('[data-label="Category"]');
       expect(category[4]).toHaveTextContent("-");
+    });
+  });
+
+  describe("when table row is clickable", () => {
+    it("should call the onClick function", () => {
+      const onClick = jest.fn();
+      render(<Table data={data} onClick={onClick} enableFiltering={true} />);
+      const row = screen.getAllByTestId("table-row")[0];
+      row.click();
+      expect(onClick).toHaveBeenCalled();
+      expect(onClick).toHaveBeenCalledWith(data[0].category);
+    });
+
+    
+  });
+
+  describe("when table row is not clickable", () => {
+    it("should not call the onClick function", () => {
+      const onClick = jest.fn();
+      render(<Table data={data} onClick={onClick} enableFiltering={false} />);
+      const row = screen.getAllByTestId("table-row")[0];
+      row.click();
+      expect(onClick).not.toHaveBeenCalled();
     });
   });
 });
